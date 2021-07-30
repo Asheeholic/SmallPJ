d20838a1a4e09e2ada841dbb11640b4d7f46b688
Han Jae Ho <chacha_95@naver.com>
2021-07-30 15:29:43 +0900

    ExamVO cp -jaeho

---
+++ myHomePage/src/main/java/co/micol/myHomePage/vo/ExamVO.java
@@ -0,0 +1,30 @@
+package co.micol.myHomePage.vo;
+
+public class ExamVO {
+	private String id;
+	private String filePath;
+	private int fId;
+	
+	public ExamVO() {
+		super();
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public int getfId() {
+		return fId;
+	}
+	public void setfId(int fId) {
+		this.fId = fId;
+	}
+	
+}

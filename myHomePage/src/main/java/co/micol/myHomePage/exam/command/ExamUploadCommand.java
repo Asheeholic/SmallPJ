d9ba388ae23d0d32487d281fd7e24463449f3703
HanJH <HanJH@HanJH-PC>
2021-07-31 14:46:40 +0900

    upload 구현 3 -jaeHo 0731 1436

--- myHomePage/src/main/java/co/micol/myHomePage/exam/command/ExamUploadCommand.java
+++ myHomePage/src/main/java/co/micol/myHomePage/exam/command/ExamUploadCommand.java
@@ -1,7 +1,10 @@
 package co.micol.myHomePage.exam.command;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,6 +25,10 @@ public class ExamUploadCommand implements Command {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO 
+		
+		//request.getParameter로 꺼낼수 없어서 map넣고 씀
+		Map<String, String> formMap = new HashMap<String, String>();
+		
 		if(ServletFileUpload.isMultipartContent(request)) {
 			DiskFileItemFactory factory = new DiskFileItemFactory();
 			factory.setSizeThreshold(MEMORY_TRESHOLD);
@@ -42,10 +49,16 @@ public class ExamUploadCommand implements Command {
 							
 							File storeFile = new File(filePath);
 							item.write(storeFile);
+							
+							
 							request.setAttribute("message", "업로드 완료됨. => "+ fileName);
 							System.out.println("동작되고 " + fileName);
 						
-						} //if
+						} else {
+							//폼을 담아줌
+							formMap.put(item.getFieldName(), item.getString("UTF-8"));
+						}
+						//if
 					} //for
 				}
 			} catch (Exception e) {
@@ -53,11 +66,23 @@ public class ExamUploadCommand implements Command {
 				e.printStackTrace();
 			}
 			
+			//맵을 통해서 폼에있는 파라미터들을 보여줌
+			for(Entry<String, String> entry : formMap.entrySet()) {
+				System.out.println("파라미터명: " + entry.getKey());
+				System.out.println("파라미터값: " + entry.getValue());
+			}
+			
+			System.out.println("꺼내짐 : " + formMap.get("sender"));
+			System.out.println("안꺼내짐 : " + request.getParameter("sender"));
+			
+			//이것이 text타입 폼을 출력해주는것.
+			request.setAttribute("sender", "보낸사람 : " + formMap.get("sender"));
+			
 		}
 		
 		
 		
-		return "exam/examUploadSuccess.jsp";
+		return "exam/examUploadSuccess";
 	}
 
 }
--- myHomePage/src/main/webapp/WEB-INF/views/exam/examUploadSuccess.jsp
+++ myHomePage/src/main/webapp/WEB-INF/views/exam/examUploadSuccess.jsp
@@ -7,6 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
+	${sender }<br>
 	${message}
 </body>
 </html>
